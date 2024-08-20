b292cdc8b167451905abdabf658a08c60b090ba6
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-08-20 23:23:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-API_ID = '21740783'
-API_HASH = 'a5dc7fec8302615f5b441ec5e238cd46'
-BOT_TOKEN = '7116266807:AAFiuS4MxcubBiHRyzKEDnmYPCRiS0f3aGU'
-SOURCE_CHANNEL_ID = -1002245327685  # Replace with the source channel ID
-TARGET_CHANNEL_ID = -1002183423252  # Replace with the target channel ID
+API_ID = ''
+API_HASH = ''
+BOT_TOKEN = ''
+SOURCE_CHANNEL_ID = -  # Replace with the source channel ID
+TARGET_CHANNEL_ID = -  # Replace with the target channel ID
 CUSTOM_CAPTION = "Watch [Anime Name] now! 🎥\n\nJoin our channel: @your_channel"
 THUMBNAIL_PATH = "Warrior Tamil.jpg"  # Path to your default thumbnail

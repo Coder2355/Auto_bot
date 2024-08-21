c55445a3c3ef4845e44a5d0593bcc7758cad6702
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-08-21 18:23:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ load_dotenv()
 API_ID = int(os.getenv("API_ID", "21740783"))
 API_HASH = os.getenv("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7116266807:AAFiuS4MxcubBiHRyzKEDnmYPCRiS0f3aGU")
+OWNER_ID = int(os.getenv("OWNER_ID", "6299192020"))
 
 # Channel IDs
 TARGET_CHANNEL_ID = int(os.getenv("TARGET_CHANNEL_ID", "-1002183423252"))  # The channel where videos will be uploaded
